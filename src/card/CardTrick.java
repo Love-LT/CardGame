09d1c509b1d04ef813443520ecceca7b4584e0c0
Love-LT <toorlo@sheridancollege.ca>
2025-01-26 18:34:38 -0500

    Update CardTrick.java
    Version 4 GitHub Lucky input added, and user input removed from code.

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -26,30 +26,50 @@ public class CardTrick {
             magicHand[i] = c;
         }
         
-        System.out.println("Pick a card between (1-13): ");
-        int value = scanner.nextInt();
+       // System.out.println("Pick a card between (1-13): ");
+       // int value = scanner.nextInt();
         
-        System.out.println("Pick a suit from (0 = Hearts, 1 = Diamonds, 2 = Spades, 3 =  Clubs");
-        int suitChoice = scanner.nextInt();
+       // System.out.println("Pick a suit from (0 = Hearts, 1 = Diamonds, 2 = Spades, 3 =  Clubs");
+       // int suitChoice = scanner.nextInt();
     
     
-    Card userChosenCard = new Card();
-    userChosenCard.setValue(value);
-    userChosenCard.setSuit(Card.SUITS[suitChoice]);
+   // Card userChosenCard = new Card();
+   // userChosenCard.setValue(value);
+   // userChosenCard.setSuit(Card.SUITS[suitChoice]);
 
-boolean isCardfound = false;
-for (Card card : magicHand) {
-    if (card.getValue() == userChosenCard.getValue() &&
-            card.getSuit().equals(userChosenCard.getSuit())) {
-            isCardfound = true;
-            break;
+Card luckyCard = new Card(); // hard coding my lucky card
+luckyCard.setValue(9);
+luckyCard.setSuit("Hearts");
+
+// boolean isCardfound = false;
+// for (Card card : magicHand) {
+   // if (card.getValue() == userChosenCard.getValue() &&
+     //       card.getSuit().equals(userChosenCard.getSuit())) {
+       //     isCardfound = true;
+         //   break;
+   // }
+// }
+
+boolean isTheLuckyCardFound = false;
+    for (Card card: magicHand) {
+    if (card.getValue() == luckyCard.getValue() &&
+        card.getSuit().equals(luckyCard.getSuit())) {
+        isTheLuckyCardFound = true;
+        break;
     }
 }
-if (isCardfound) {
-    System.out.println("Awesome! Your card is in the magic hand.");
-} else {
-    System.out.println("Sorry, your card was not in the magic hand");
-}
+
+
+// if (isCardfound) {
+   // System.out.println("Awesome! Your card is in the magic hand.");
+// } else {
+   // System.out.println("Sorry, your card was not in the magic hand");
+// }
+            if (isTheLuckyCardFound) {
+                System.out.println("Your luck is at the highest, the lucky card is in the magic hand :) ");
+            } else {
+                System.out.println("The lucky card is not in the magic hand :( ");
+            }
         }
     }
 }
