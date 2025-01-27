30004b9c5237de83a5d9cb2e4c3d0b7cf267953b
Love-LT <toorlo@sheridancollege.ca>
2025-01-26 20:45:21 -0500

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// Name: Lovepreet Toor
+// Student Number: 991803976
 package card;
 
 /**
